f93e242b7918bf53fdd936fa3cb328f2629b6ca9
Manuchehr <manuchekhr.3232@gmail.com>
2026-01-23 09:21:54 +0500

    config neovim cord

--- nvim/lua/plugins/fun/cord.lua
+++ nvim/lua/plugins/fun/cord.lua
@@ -8,5 +8,26 @@ return {
       tooltip = "i use vim btw",
       icon = nil,
     },
+
+    text = {
+      default = "Coding...",
+      workspace = nil,
+      viewing = nil,
+      editing = nil,
+      file_browser = "Browsing files",
+      plugin_manager = "Managing plugins",
+      lsp = "Configuring LSP",
+      docs = function(opts)
+        return "Reading " .. opts.name
+      end,
+      vcs = nil,
+      notes = nil,
+      debug = nil,
+      test = nil,
+      diagnostics = nil,
+      games = nil,
+      terminal = nil,
+      dashboard = nil,
+    },
   },
 }
